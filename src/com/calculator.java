98a6301632d721a2ce8ef7426bc79ed6bbbd085c
Pravalllika <pravallikaraonandhiwada@gmail.com>
2023-02-28 19:59:16 +0530

    adding eclipse project in local repo

---
+++ src/com/calculator.java
@@ -0,0 +1,28 @@
+package com;
+
+public class calculator {
+	public int add(int num1,int num2) {
+		int sum;
+		sum=num1+num2;
+		return sum;
+	}
+	public int sub(int num1,int num2) {
+		int sub;
+		sub=num1-num2;
+		return sub;
+	}
+	public int mul(int num1, int num2) {
+		int mul;
+		mul=num1*num2;
+		return mul;
+	}
+	public int div(int num1,int num2) {
+		int div;
+		div=num1/num2;
+		return div;
+	
+		
+		
+	}
+
+}
---
+++ src/com/solution.java
@@ -0,0 +1,11 @@
+package com;
+
+public class solution {
+public static void main(String args[]) {
+ calculator calc =new calculator();
+	System.out.println(calc.add(56,87));
+	System.out.println(calc.sub(98,34));
+	System.out.println(calc.mul(45,23));
+	System.out.println(calc.div(108,9));
+}
+}
